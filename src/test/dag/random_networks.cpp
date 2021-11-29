be87840ee63960753df5c614329f8ae15a1ff92c
Arash Badie Modiri <arashbm@gmail.com>
2021-11-29 22:47:37 +0200

    add random k-regular graphs

--- src/test/dag/random_networks.cpp
+++ src/test/dag/random_networks.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <algorithm>
 
 #include <catch2/catch.hpp>
 
@@ -22,9 +23,22 @@ TEST_CASE("G(n, p) random graph", "[dag::gnp_random_graph]") {
 
 TEST_CASE("Barabasi-Albert random graph", "[dag::ba_random_graph]") {
   std::mt19937_64 gen(42);
-  std::size_t n = 1000, m = 3;
+  std::size_t n = 100, m = 3;
   dag::undirected_network<std::size_t> r =
     dag::ba_random_graph<std::size_t>(n, m, gen);
   REQUIRE(r.vertices().size() == n);
   REQUIRE(r.edges().size() == (n-m)*m);
+}
+
+TEST_CASE("random k-regular graph", "[dag::random_regular_graph]") {
+  std::mt19937_64 gen(42);
+  std::size_t n = 10, k = 4;
+  dag::undirected_network<std::size_t> r =
+    dag::random_regular_graph<std::size_t>(n, k, gen);
+  REQUIRE(r.vertices().size() == n);
+  REQUIRE(r.edges().size() == k*n/2);
+
+  auto verts = r.vertices();
+  REQUIRE(std::all_of(verts.cbegin(), verts.cend(),
+        [&r, k](std::size_t v) { return r.degree(v) == k; }));
 }
