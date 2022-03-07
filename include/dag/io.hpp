e66e999f7652697782115d1520414c02f9fe4f0a
Arash Badie-Modiri <arashbm@gmail.com>
2022-03-07 12:39:59 +0200

    ignore extern directory in CPPLint

--- include/dag/io.hpp
+++ include/dag/io.hpp
@@ -10,31 +10,31 @@ namespace dag {
   template <network_vertex VertT>
   undirected_network<VertT>
   read_undirected_edgelist(std::filesystem::path path,
-          char delimiter=' ', char quote='"');
+          char delimiter = ' ', char quote = '"');
 
   template <network_vertex VertT>
   directed_network<VertT>
   read_directed_edgelist(
           std::filesystem::path path,
-          char delimiter=' ', char quote='"');
+          char delimiter = ' ', char quote = '"');
 
   template <network_vertex VertT, typename TimeT>
   undirected_temporal_network<VertT, TimeT>
   read_undirected_eventlist(std::filesystem::path path,
-          char delimiter=' ', char quote='"');
+          char delimiter = ' ', char quote = '"');
 
   template <network_vertex VertT, typename TimeT>
   directed_temporal_network<VertT, TimeT>
   read_directed_eventlist(std::filesystem::path path,
-          char delimiter=' ', char quote='"');
+          char delimiter = ' ', char quote = '"');
 
   template <network_vertex VertT, typename TimeT>
   directed_delayed_temporal_network<VertT, TimeT>
   read_directed_eventlist(std::filesystem::path path,
-          char delimiter=' ', char quote='"');
+          char delimiter = ' ', char quote = '"');
 }  // namespace dag
 
 #include "../../src/io.tpp"
 
-#endif // INCLUDE_DAG_IO_HPP_
+#endif  // INCLUDE_DAG_IO_HPP_
 
