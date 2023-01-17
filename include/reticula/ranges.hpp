f8084cbb004aa822db6d23e82a5dacb649c64f28
Arash Badie Modiri <arashbm@gmail.com>
2023-01-18 01:40:21 +0200

    trying libstdc++ for MacOS

--- include/reticula/ranges.hpp
+++ include/reticula/ranges.hpp
@@ -1,22 +1,11 @@
 #ifndef INCLUDE_RETICULA_RANGES_HPP_
 #define INCLUDE_RETICULA_RANGES_HPP_
 
-#ifdef RETICULA_SHIM_RANGES
-
-#include <range/v3/all.hpp>
-namespace reticula {
-  namespace ranges = ::ranges::cpp20;
-  namespace views = ::ranges::cpp20::views;
-}  // namespace reticula
-
-#else
-
 #include <ranges>
+
 namespace reticula {
   namespace ranges = std::ranges;
   namespace views = std::ranges::views;
 }  // namespace reticula
-
-#endif  // RETICULA_SHIM_RANGES
 
 #endif  // INCLUDE_RETICULA_RANGES_HPP_
