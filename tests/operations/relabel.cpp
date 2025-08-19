aa600f554d826c73100be9b035e33bd28824b74c
Arash Badie-Modiri <arashbm@gmail.com>
2025-08-19 23:10:24 +0300

    include std::map

--- tests/operations/relabel.cpp
+++ tests/operations/relabel.cpp
@@ -1,7 +1,9 @@
-#include "reticula/concepts.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_range_equals.hpp>
 
+#include <map>
+
+#include <reticula/concepts.hpp>
 #include <reticula/networks.hpp>
 #include <reticula/operations/relabel.hpp>
 
